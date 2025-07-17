b0b13b501d766daf655502f379232ef618f4801f
mern-mechanic <abhishek.kumar2526@gmail.com>
2025-07-17 11:29:44 +0530

    Initial commit from my new laptop

--- codeMateWeb/src/components/udemy/playground.js
+++ codeMateWeb/src/components/udemy/playground.js
@@ -1,23 +1,2 @@
 console.clear();
-console.log('<------------------ CODE -------------------->');
-
-const findLastIndexOfZero = (arr) => {
-    let left = 0;
-    let right = arr.length - 1;
-    let lastIndex = -1;
-
-    while (left < right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (arr[mid] === 0) {
-            lastIndex = mid;
-            left = mid;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return lastIndex;
-};
-
-console.log(findLastIndexOfZero([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+console.log('<------------------ नमस्ते -------------------->');
--- codeMateWeb/src/components/udemy/revision/sameFrequencyInNumber.js
+++ codeMateWeb/src/components/udemy/revision/sameFrequencyInNumber.js
@@ -1,24 +1,26 @@
+const getNumberFrequency = (num) => {
+    let obj = {};
+
+    while (num > 0) {
+        let digit = num % 10;
+        obj[digit] = (obj[digit] || 0) + 1;
+        num = Math.floor(num / 10);
+    }
+
+    return obj;
+};
+
 const sameFrequencyInNumber = (num1, num2) => {
-    const num1Frequency = {};
-    const num2Frequency = {};
+    let num1Frq = getNumberFrequency(num1);
+    let num2Frq = getNumberFrequency(num2);
 
-    while (num1 > 0) {
-        const rem = num1 % 10;
-        num1Frequency[rem] = (num1Frequency[rem] || 0) + 1;
-        num1 = Math.floor(num1 / 10);
-    }
-
-    while (num2 > 0) {
-        const rem = num2 % 10;
-        num2Frequency[rem] = (num2Frequency[rem] || 0) + 1;
-        num2 = Math.floor(num2 / 10);
-    }
-
-    for (let key in num1Frequency) {
-        if (num1Frequency[key] !== num2Frequency[key]) return false;
+    for (let key in num1Frq) {
+        if (num1Frq[key] !== num2Frq[key]) return false;
     }
 
     return true;
 };
+
+console.log(sameFrequencyInNumber(18233, 2813));
 
 console.log(sameFrequencyInNumber(182, 281));
